9e31d93048ff4d7440b6e164e4db1ddf181b36b9
Rahul R Mody <rrm43@njit.edu>
2018-03-02 07:47:21 -0500

    Completed AS1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<h1> Welcome to the to-do list Application</h1>
+	</head>
+	<body>
+		<p class="p1">
+			<h2>Heading 2</h2>
+		</p>
+		<p class="p2">
+			<h3>Head 3</h3>
+			<h4>H4 </h4>
+		</p>
+	</body>
+
+</html>
